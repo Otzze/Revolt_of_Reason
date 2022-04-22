736637fe8269b345b4466dfc31b89b8b350cf638
Hippolyte <Hippolyte.evva@epita.fr>
2022-04-22 22:33:37 +0200

    add Resources/PNJ and Script/NPCIA

---
+++ Assets/Scripts/PNJ_script/NPCAI.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCAI : MonoBehaviour
+{
+    public GameObject destinationPoint;
+    private NavMeshAgent theAgent;
+    void Start()
+    {
+        theAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        theAgent.SetDestination(destinationPoint.transform.position);
+    }
+}
